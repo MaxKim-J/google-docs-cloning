98cde1abb1415e4b3988c0955e85dacebb1b92c2
bumsu <bumsu0211@gmail.com>
2019-09-10 20:30:24 +0900

    Modify views.py. delete print

--- products/views.py
+++ products/views.py
@@ -2,11 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def docs_home(request):
-    print("docs")
     return render(request, 'products/docs_home.html')
 
 def sheets_home(request):
-    print("sheets")
     return render(request, 'products/sheets_home.html')
 
 def slides_home(request):
